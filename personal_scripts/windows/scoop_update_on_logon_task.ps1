0e1ebeaff550694119772133fa32fb6343f8ec32
Paramvir Das <paramvir2307@gmail.com>
2024-08-20 19:55:10 +0200

    update logon_task script
    scoop_update_on_logon_task.ps1 is a script which sets up a scheduled task (on user logon) to update scoop and all its apps and afterwards start powertoys, since powertoys can't be updated while running

--- personal_scripts/windows/scoop_update_on_logon_task.ps1
+++ personal_scripts/windows/scoop_update_on_logon_task.ps1
@@ -1,5 +1,5 @@
 $user = "param"
-$actions = (New-ScheduledTaskAction -Execute 'C:\Users\param\OneDrive\Documents\scoop_update.bat'), (New-ScheduledTaskAction -Execute 'C:\Users\param\scoop\apps\powertoys\current\PowerToys.exe')
+$actions = (New-ScheduledTaskAction -Execute "$PSScriptRoot\scoop_update.bat"), (New-ScheduledTaskAction -Execute 'C:\Users\param\scoop\apps\powertoys\current\PowerToys.exe')
 $trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay 00:01:00 -User $user
 $taskPath = "\CustomTasks\"
 $taskName = "UpdateScoopAndStartPowerToys"
