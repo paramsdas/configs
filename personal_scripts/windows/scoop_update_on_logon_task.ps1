8138d5499e964e214e239f6427d8fd2a30368e64
Paramvir Das <paramvir2307@gmail.com>
2024-09-04 23:13:33 +0200

    updated logon/startup script
    updated the script to use only the batch file and made sure that the VPN starts after the updates have already been carried out

--- personal_scripts/windows/scoop_update_on_logon_task.ps1
+++ personal_scripts/windows/scoop_update_on_logon_task.ps1
@@ -1,5 +1,5 @@
 $user = "param"
-$actions = (New-ScheduledTaskAction -Execute "$PSScriptRoot\scoop_update.bat"), (New-ScheduledTaskAction -Execute 'C:\Users\param\scoop\apps\powertoys\current\PowerToys.exe')
+$actions = (New-ScheduledTaskAction -Execute "$PSScriptRoot\scoop_update.bat")
 $trigger = New-ScheduledTaskTrigger -AtLogOn -RandomDelay 00:00:30 -User $user
 $taskPath = "\CustomTasks\"
 $taskName = "UpdateScoopAndStartPowerToys"
