79725cc5c6f6d1bbc24a9a84e914dd2b29ae1c8c
Paramvir Das <paramvir2307@gmail.com>
2024-09-05 07:55:34 +0200

    remove pause from batch script

--- personal_scripts/windows/scoop_update.bat
+++ personal_scripts/windows/scoop_update.bat
@@ -1,1 +1,1 @@
-scoop update && scoop update -a && start /d "C:\Users\param\scoop\apps\powertoys\current" PowerToys.exe && start /d "C:\Program Files\NordVPN" NordVPN.exe && pause
+scoop update && scoop update -a && start /d "C:\Users\param\scoop\apps\powertoys\current" PowerToys.exe && start /d "C:\Program Files\NordVPN" NordVPN.exe
